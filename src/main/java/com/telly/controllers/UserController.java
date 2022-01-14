1403dbbc82e41bb2cc20d76c4d89195d086b948d
lynaBoukari <il_boukari@esi.dz>
2022-01-14 17:09:20 +0100

    Affichage des  reservation

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -1,3 +1,21 @@
+@RequestMapping(value = "/reservebook", method = RequestMethod.POST)
+public String createReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, BindingResult result, Principal principal) {
+
+        if (result.hasErrors()) {
+        return "reservebus";
+        }
+
+        String username = principal.getName();
+        reserve.getUser().setUsername(username);
+
+        reserveService.reserve(reserve);
+
+
+        return "home";
+
+        }
+
+
 @RequestMapping(value = "/getreservations", method = RequestMethod.GET)
 public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
 
