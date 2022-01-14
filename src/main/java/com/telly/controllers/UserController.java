11c549ffe165ab96ced14fa06998c7f96441db4f
Sarra BOUZOUL <is-bouzoul@esi.dz>
2022-01-14 16:58:15 +0100

    deconnexion

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -3,11 +3,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
+
 	@RequestMapping("/login")
 	public String showLogin() {
 		return "login";
 	}
 
+	@RequestMapping("/loggedout")
+	public String showLogout() {
+		return "loggedout";
+	}
 
 }
 
