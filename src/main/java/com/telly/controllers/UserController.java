44616efce0df2c8b088f95e263ee8383148612be
lynaBoukari <il_boukari@esi.dz>
2022-01-14 17:26:31 +0100

    resultion du conflit

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -1,8 +1,39 @@
-@RequestMapping(value = "/reservebook", method = RequestMethod.POST)
-public String createReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, BindingResult result, Principal principal) {
+package com.telly.controllers;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.telly.dao.FormValidationGroup;
+import com.telly.dao.Reserve;
+import com.telly.dao.User;
+import com.telly.service.ReserveService;
+import com.telly.service.UserService;
+
+
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    ReserveService reserveService;
+
+
+    @RequestMapping(value = "/reservebook", method = RequestMethod.POST)
+    public String createReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, BindingResult result, Principal principal) {
 
         if (result.hasErrors()) {
-        return "reservebus";
+            return "reservebus";
         }
 
         String username = principal.getName();
@@ -13,11 +44,11 @@ public String createReserveBook(@Validated(FormValidationGroup.class) Reserve re
 
         return "home";
 
-        }
+    }
 
 
-@RequestMapping(value = "/getreservations", method = RequestMethod.GET)
-public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
+    @RequestMapping(value = "/getreservations", method = RequestMethod.GET)
+    public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, Model model, Principal principal) {
 
 
         String username = principal.getName();
@@ -30,7 +61,5 @@ public String getReserveBook(@Validated(FormValidationGroup.class) Reserve reser
 
         return "home";
 
-        }
-
-
-        }
+    }
+}
